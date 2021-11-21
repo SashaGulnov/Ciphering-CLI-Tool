5b04f34187e323c66939e0ed21a37d1d91a4fb35
SashaGulnov <kirill_titarenko98@mail.ru>
2021-11-21 13:55:06 +0300

    frat: add encription tests

--- testing/encription/atbashEncription.test.js
+++ testing/encription/atbashEncription.test.js
@@ -3,11 +3,22 @@ const _ = require('../../globals');
 
 describe('Atbash Encription: ', () => {
 
-  let text = 'This is secret. Message about "_" symbol!'.split('');
+  let text = 'This is secret. Message about "_" symbol!';
+  let result = 'Gsrh rh hvxivg. Nvhhztv zylfg "_" hbnylo!';
 
-  test('should be instanse of Array', () => {
-    expect(atbashEncription(text)).toBeInstanseOf(Array);
+  test('should have proper result', () => {
+    expect(atbashEncription(text.split(''))).toBe(result);
   })
+  
+  test('should have proper reversed result', () => {
+    expect(atbashEncription(result.split(''))).toBe(text);
+  })
+
+  test('should be falsy', () => {
+    expect(atbashEncription(''.split(''))).toBeFalsy();
+  })
+
+
 
 
 })
